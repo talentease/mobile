5942cb2f852dcc177f2fd281674d674e044942dc
fnakhsan <fn.akhsan@gmail.com>
2023-06-16 21:49:02 +0700

    clean project

--- app/src/main/java/com/bangkit/c23pr492/talentease/di/Injection.kt
+++ app/src/main/java/com/bangkit/c23pr492/talentease/di/Injection.kt
@@ -7,7 +7,6 @@ import androidx.datastore.preferences.preferencesDataStore
 import com.bangkit.c23pr492.talentease.data.AuthRepository
 import com.bangkit.c23pr492.talentease.data.RecruiterRepository
 import com.bangkit.c23pr492.talentease.data.TalentRepository
-import com.bangkit.c23pr492.talentease.data.database.TalentEaseDatabase
 import com.bangkit.c23pr492.talentease.data.datastore.AuthDataStore
 import com.bangkit.c23pr492.talentease.data.network.ApiConfig
 import com.google.firebase.auth.FirebaseAuth
@@ -31,8 +30,6 @@ object Injection {
     }
 
     fun provideTalentRepository(context: Context): TalentRepository {
-        val database = TalentEaseDatabase.getDatabase(context)
-        val dao = database.talentEaseDao()
         return TalentRepository.getInstance(
             ApiConfig.getApiService(),
             AuthDataStore.getInstance(context.dataStore)
